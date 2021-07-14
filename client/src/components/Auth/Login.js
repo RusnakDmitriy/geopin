c80d673f74f197c2cddf8a5f6f6eba1f5aaf2e05
RusnakDmitriy <rusnak179@gmail.com>
2021-07-14 20:10:05 +0300

    store current user in react context

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { GoogleLogin } from 'react-google-login';
 import { GraphQLClient } from 'graphql-request';
 // import Typography from "@material-ui/core/Typography";
+
+import Context from '../../context';
 
 const ME_QUERY = `
   {
@@ -16,13 +18,18 @@ const ME_QUERY = `
 `;
 
 const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context);
+
   const responseGoogle = async googleUser => {
     const idToken = googleUser.getAuthResponse().id_token;
     const client = new GraphQLClient(process.env.REACT_APP_BASE_API_URL, {
       headers: { authorization: idToken }
     });
     const data = await client.request(ME_QUERY);
-    console.log(data)
+    dispatch({
+      type: 'LOGIN_USER',
+      payload: data.me
+    });
   };
 
   const rejectGoogle = (err) => console.error(err);
