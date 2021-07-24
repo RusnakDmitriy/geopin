89880a6c9374a324eeee30e6e2225122b5e03c07
RusnakDmitriy <rusnak179@gmail.com>
2021-07-24 15:46:03 +0300

    add .env.example files

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ async function start() {
     mongoose
       .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('DB connected!');
-    const { url } = await server.listen();
+    const { url } = await server.listen({ port: process.env.PORT || 4000 });
     console.log(`Server is listening on ${url}`);
   } catch (err) {
     console.error(err)
